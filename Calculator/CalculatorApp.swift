25d126b968433bee2d4cff62fbf72aae91ca6d32
Amshu Wagle <amshuw05@gmail.com>
2024-09-26 21:15:05 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Amshu Wagle on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
